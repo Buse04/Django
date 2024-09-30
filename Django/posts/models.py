312472a572a1299439caf2d41f50d84c18728b34
Buse04 <busebaylav1@gmail.com>
2024-09-30 16:17:47 +0300

    last commit

--- Django/posts/models.py
+++ Django/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # When you add new models here, you need to type makemigration commnet on terminal
@@ -9,6 +10,7 @@ class Post(models.Model):
     slug = models.SlugField()
     date_time = models.DateTimeField(auto_now_add=True)
     banner = models.ImageField(default='fallback.png', blank=True)
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    
     def __str__(self):
         return self.title
